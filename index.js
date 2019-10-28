5e97f5d6cb7165a28f4086d9c1bd65c624880974
G H <gh@Gs-MacBook-Pro.local>
2019-10-28 13:51:42 -0700

    find

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+const server = express();
+
+//middleware
+
+server.use(express.json());
+
+server.get('/db', (req, res) =>{
+db.find()
+  .then(db => {
+      res.status(200).json(db);
+  })
+  .catch(err => {
+      console.log('error', err);
+      res.status(500).json({error: 'failed to get hubs from db'});
+
+  });
+});
+const port = 8080;
+server.listen(port, () => console.log('\n=== API on port 8080 ===\n'));
